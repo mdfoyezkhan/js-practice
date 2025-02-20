3ee7ed407fa92588c5e3d406525721b676102919
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-02-20 23:37:04 +0600

    some code added this commit

--- milestone-5/module-25/dom/dom.js
+++ milestone-5/module-25/dom/dom.js
@@ -1,2 +1,13 @@
 console.log('hello javascript');
 console.log(document)
+console.log(document.getElementsByTagName('li.innerText'))
+const liCollection =document.getElementsByTagName('li');
+// console.log(liCollection)
+for(const li of liCollection){
+    console.log(li.innerText)
+}
+const heading = document.getElementsByTagName('h1');
+// console.log(heading.innerText)
+for(const h1 of heading){
+    console.log(h1.innerText)
+}
