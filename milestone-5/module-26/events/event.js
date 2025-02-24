3bfda0c2edf6a19d72bf6f124628318ed72ca187
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-02-24 22:52:43 +0600

    some code added for bg change

--- milestone-5/module-26/events/event.js
+++ milestone-5/module-26/events/event.js
@@ -3,8 +3,27 @@ clickMe.onclick= makeRed
 function makeRed(){
     document.body.style.backgroundColor ='red';
 }
-// makeRed()
+// makeRed()[we will use this sometime]
 const mrb = document.getElementById('mrb');
-mrb.onclick = function foyej(){
+mrb.onclick = function makeGreen(){
     document.body.style.backgroundColor = 'green';
+    console.log('hello world')
 }
+
+// options 4
+const pinkButton = document.getElementById('pink');
+       pinkButton.addEventListener('click',pinkbg);
+       function pinkbg(){
+        // document.body.style.backgroundColor ='pink';
+       }
+// options 4 anther
+const pinkButton2 = document.getElementById('pink');
+pinkButton2.addEventListener('click',function(){
+    document.body.style.backgroundColor ='pink';
+})
+
+// options 4 final
+// important : we will use this sometimes
+document.getElementById('golden').addEventListener('click',function(){
+    document.body.style.backgroundColor = 'goldenrod'
+   })
