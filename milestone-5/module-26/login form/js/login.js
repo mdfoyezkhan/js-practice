58a40f92984454b9ee314e35fcc94f1af54be377
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-02-28 00:53:25 +0600

    some code added in this commit j

--- milestone-5/module-26/login form/js/login.js
+++ milestone-5/module-26/login form/js/login.js
@@ -1,16 +1,31 @@
+// document.getElementById('button-id').addEventListener('click', function(event){
+//     event.preventDefault();
+//     console.log('hello world')
+//     // Number input 
+//     const phoneNumber = document.getElementById('Phone-number').value;
+//     console.log(phoneNumber)
+//     // pin input
+//     const pinNumber = document.getElementById('pin-number').value;
+//     console.log(pinNumber)
+//     if(phoneNumber === '11' && pinNumber === 'foyejkhan'){
+//         console.log('foyej khan welcom to my website');
+//         window.Location.href = 'js/home.html'
+//     }
+//     else{
+//         alert('wrong phone number or pin')
+//     }
+// })
 document.getElementById('button-id').addEventListener('click', function(event){
     event.preventDefault();
-    console.log('hello world')
-    // Number input 
     const phoneNumber = document.getElementById('Phone-number').value;
-    console.log(phoneNumber)
-    // pin input
+    // console.log(phoneNumber);
     const pinNumber = document.getElementById('pin-number').value;
-    console.log(pinNumber)
-    if(phoneNumber === '11' && pinNumber === 'foyejkhan'){
-        console.log('foyej khan welcom to my website');
+    // console.log(pinNumber)
+    if(phoneNumber === '11' && pinNumber === '1234'){
+        console.log('you are login');
+        window.location.href ='/home.html';
     }
     else{
-        alert('wrong phone number or pin')
+        alert('somthing wont wrong')
     }
 })
