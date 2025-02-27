19fef6cc9b40aab394d654dc40b481563bc0676f
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-02-27 23:49:58 +0600

    some code added in this commit

--- milestone-5/module-26/login form/js/login.js
+++ milestone-5/module-26/login form/js/login.js
@@ -4,4 +4,13 @@ document.getElementById('button-id').addEventListener('click', function(event){
     // Number input 
     const phoneNumber = document.getElementById('Phone-number').value;
     console.log(phoneNumber)
+    // pin input
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(pinNumber)
+    if(phoneNumber === '11' && pinNumber === 'foyejkhan'){
+        console.log('foyej khan welcom to my website');
+    }
+    else{
+        alert('wrong phone number or pin')
+    }
 })
