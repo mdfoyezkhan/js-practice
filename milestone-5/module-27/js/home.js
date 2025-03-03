d2ed82cf680514e473a7be5116e46529bd22402a
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-03-03 23:24:15 +0600

    some code added in this commit

--- milestone-5/module-27/js/home.js
+++ milestone-5/module-27/js/home.js
@@ -1,6 +1,20 @@
 document.getElementById('add-money').addEventListener('click', function(event){
-event.preventDefault();
-const inputForAmount = document.getElementById('input-for-amount').value;
-const inputForPin = document.getElementById('input-for-pin').value;
-console.log(inputForAmount,inputForPin)
-})
+    event.preventDefault();
+    const inputForAmount = document.getElementById('input-for-amount').value;
+    const inputForPin = document.getElementById('input-for-pin').value;
+    
+    if(inputForPin === '1234'){
+        const balance = document.getElementById('balance').innerText;
+        console.log(balance)
+        // const newBalance = balance + inputForAmount;
+        const inputAmountForIntiger = parseFloat(inputForAmount);
+        const balanceForIntiger = parseFloat(balance);
+        const totalBalance = balanceForIntiger + inputAmountForIntiger;
+        document.getElementById('balance').innerText = totalBalance;
+        
+        
+    }
+    else{
+        alert('something wont wrong! try again')
+    }
+    })
