40430fa3979c3cadfae91e842196291f5b522667
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-03-04 22:41:29 +0600

    some code added in this commit

---
+++ milestone-5/module-27/js/cashout.js
@@ -0,0 +1,17 @@
+document.getElementById('cashout').addEventListener('click', function(event){
+    event.preventDefault();
+    const inputForAmount = document.getElementById('input-for-amount').value;
+    const inputfloat = parseFloat(inputForAmount);
+    const inputForPin = document.getElementById('input-for-pin').value;
+    
+    if(inputForPin === 'foyej'){
+        const balance = document.getElementById('balance').innerText;
+        console.log('hello',balance)
+        const newBalance = parseFloat(balance);
+        const totalBalance = newBalance + inputfloat;
+        document.getElementById('balance').innerText = totalBalance;
+    }
+    else{
+        alert('something wont wron')
+    }
+})
