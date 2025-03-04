31296f61a04e519c03f04a9bd06ffbae89798a84
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-03-04 22:52:50 +0600

    some code added

--- milestone-5/module-27/js/cashout.js
+++ milestone-5/module-27/js/cashout.js
@@ -1,14 +1,14 @@
 document.getElementById('cashout').addEventListener('click', function(event){
     event.preventDefault();
     const inputForAmount = document.getElementById('input-for-amount').value;
-    const inputfloat = parseFloat(inputForAmount);
+    const inputfloats = parseFloat(inputForAmount);
     const inputForPin = document.getElementById('input-for-pin').value;
     
     if(inputForPin === 'foyej'){
         const balance = document.getElementById('balance').innerText;
         console.log('hello',balance)
         const newBalance = parseFloat(balance);
-        const totalBalances = newBalance + inputfloat;
+        const totalBalances = newBalance + inputfloats;
         document.getElementById('balance').innerText = totalBalances;
     }
     else{
