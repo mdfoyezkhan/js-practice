77748656f250d7a3e707930eda1a9fb478d2999f
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-03-05 22:54:52 +0600

    new file added in this commit

---
+++ milestone-5/module-27/mini-project/js/mini.js
@@ -0,0 +1,15 @@
+document.getElementById('Login-btn').addEventListener('click',function(event){
+    event.preventDefault();
+    const phoneNumber = document.getElementById('Phone-Number').value;
+    // const phoneNumberForFloat = parseFloat(phoneNumber);
+    const yourPin = document.getElementById('your-pin').value;
+    console.log(typeof phoneNumberForFloat)
+    if(yourPin === '1234' && phoneNumber === '01771944422'){
+        console.log('your logged')
+        window.location.href = 'home.html'
+    }
+    else{
+        alert('something wont wrong')
+    }
+    
+})
