266234721c8d9e25cc6771387a29fb6b9e88836c
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-03-06 23:11:47 +0600

    new js code added in this commit

--- milestone-5/module-28/script.js
+++ milestone-5/module-28/script.js
@@ -1,6 +1,15 @@
 document.getElementById('add-money-btn').addEventListener('click',function(event){
     event.preventDefault();
     const amount = document.getElementById('Amount').value;
+    const amountConvart = parseFloat(amount);
     const pinNumber = document.getElementById('pin-number').value;
-    console.log(amount,pinNumber)
+    if(pinNumber === '1234'){
+        const balanec = document.getElementById('Balance').innerText;
+        const balanceConvart = parseFloat(balanec);
+        const totalBalance = amountConvart + balanceConvart;
+        document.getElementById('Balance').innerText = totalBalance;
+    }
+    else{
+        alert('try again')
+    }
 })
