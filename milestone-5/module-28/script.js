0345931f865b6004df9987c9dfd30183b76ade53
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-03-06 22:51:29 +0600

    some js code added in this commit

--- milestone-5/module-28/script.js
+++ milestone-5/module-28/script.js
@@ -1,1 +1,6 @@
-console.log('hello world')
+document.getElementById('add-money-btn').addEventListener('click',function(event){
+    event.preventDefault();
+    const amount = document.getElementById('Amount').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(amount,pinNumber)
+})
