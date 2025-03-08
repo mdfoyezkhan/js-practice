f63da759fbbfa47db27628801c49472972d07257
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-03-08 22:01:46 +0600

    some code added in this commit

--- milestone-5/module-27/mini-project/js/mini.js
+++ milestone-5/module-27/mini-project/js/mini.js
@@ -13,3 +13,21 @@ document.getElementById('Login-btn').addEventListener('click',function(event){
     }
     
 })
+
+// 
+document.getElementById('Login-btn').addEventListener('click',function(event){
+    event.preventDefault();
+    const phoneNumber = document.getElementById('Phone-Number').value;
+    // const phoneNumberForFloat = parseFloat(phoneNumber);
+    const yourPin = document.getElementById('your-pin').value;
+    console.log(typeof phoneNumberForFloat)
+    if(yourPin === '1234' && phoneNumber === '01771944422'){
+        console.log('your logged')
+        console.log('how are you all')
+        window.location.href = 'home.html'
+    }
+    else{
+        alert('something wont wrong')
+    }
+    
+})
