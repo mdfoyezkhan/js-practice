a7f24d60c68440042d3341ff0f64dba37b1b3fe1
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-03-09 15:17:35 +0600

    some code added this commit

--- milestone-5/module-28/payoo/addmoney.js
+++ milestone-5/module-28/payoo/addmoney.js
@@ -1,3 +1,8 @@
 document.getElementById('add-money-btn').addEventListener('click',function(event){
-    event.preventDefault()
+    event.preventDefault();
+    // const addMoney = getInputFieldValueById();
+    // console.log('add money value', addMoney);
+    const inputAddNumber = getInputFieldValueById('Amount-input');
+    const inputValue = getInputFieldValueById('pin-number-input')
+    console.log(inputAddNumber, inputValue)
 })
--- milestone-5/module-28/payoo/utilites.js
+++ milestone-5/module-28/payoo/utilites.js
@@ -1,5 +1,11 @@
 console.log('utilites file added')
 
-function getInputFieldValueById(){
-    console.log('will get value by id')
+// function getInputFieldValueById(){
+//     console.log('will get value by id');
+//     const amountAddMoney = document.getElementById('Amount-input').value;
+//     return amountAddMoney;
+// }
+function getInputFieldValueById(id){
+    const inputValue = document.getElementById(id).value;
+    return inputValue;
 }
