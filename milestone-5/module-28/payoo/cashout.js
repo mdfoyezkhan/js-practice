71f91852658a8ddfe84650290a59bc2a9fcf84ff
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-03-12 00:51:47 +0600

    some code added in this commit

--- milestone-5/module-28/payoo/cashout.js
+++ milestone-5/module-28/payoo/cashout.js
@@ -2,5 +2,12 @@ document.getElementById('cash-out-btn').addEventListener('click',function(event)
     event.preventDefault();
     const cashOutNumber = getInputFieldValueById('cash-out-Amount-input');
     const cashOutPin = getInputFieldValueById('cash-pin-number-input');
-    console.log('event hendler added',cashOutNumber, cashOutPin)
+   if(cashOutPin === 1234){
+    const cashOutBalance = getTextFieldValueById('balance');
+    const availableBalance = cashOutBalance - cashOutNumber;
+    document.getElementById('balance').innerText = availableBalance;
+   }
+   else{
+    alert('something wont rong')
+   }
 })
