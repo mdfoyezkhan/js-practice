4af187421fc87436650eb792213a167ee3f54ead
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-03-17 23:25:33 +0600

    some code added in this commit

--- milestone-6/module-30/var-let-const.js
+++ milestone-6/module-30/var-let-const.js
@@ -1,3 +1,6 @@
-const money = 40;
-const rich = money + 50;
-console.log(rich)
+// const money = 40;
+// const rich = money + 50;
+// console.log(rich)
+let money = 40;
+money = 70;
+console.log(money)
