bbc7353e099d2b64d00ab85ceed1291114d17d6c
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-03-18 00:01:37 +0600

    function code added

---
+++ milestone-6/module-30/defult.js
@@ -0,0 +1,11 @@
+function add(num1 = 10, num2 = 10){
+    const result = num1 + num2;
+    console.log(num1, num2, result)
+    return result
+}
+// add(10, 10);
+add();
+function friends(numbs =[]){
+
+}
+friends()
