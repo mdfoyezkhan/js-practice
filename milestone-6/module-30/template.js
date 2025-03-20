2ae757491025269adf7c34c9475a95ae673d8319
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-03-20 22:45:54 +0600

    some code added

--- milestone-6/module-30/template.js
+++ milestone-6/module-30/template.js
@@ -5,5 +5,11 @@ const name = first + ' ' + last + ' ' + friend;
 console.log(name)
 const a = 30;
 const b = 20;
-const result = 'the sum of ' + a + 'and ' + b + ' is' + (a + b);
+const result = 'the sum of ' + a + 'and ' + b + ' is ' + (a + b);
+const results = `the sum of ${a} and ${b} is ${a +b}`;
 console.log(result)
+console.log(results)
+const p = `hi 
+foyej khan
+how are you`;
+console.log(p)
