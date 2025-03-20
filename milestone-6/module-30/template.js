0ccacbe5d055f89e2d82db13d494965513ed06ed
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-03-20 22:36:46 +0600

    some code added in this commit

--- milestone-6/module-30/template.js
+++ milestone-6/module-30/template.js
@@ -3,3 +3,7 @@ const last = 'khan';
 const friend = 'salman khan';
 const name = first + ' ' + last + ' ' + friend;
 console.log(name)
+const a = 30;
+const b = 20;
+const result = 'the sum of ' + a + 'and ' + b + ' is' + (a + b);
+console.log(result)
