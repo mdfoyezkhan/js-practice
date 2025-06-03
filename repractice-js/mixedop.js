759715ebaa4b66ae024398f9f7d210cabb56c99a
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-06-03 23:35:23 +0600

    some concat code added in this commit

---
+++ repractice-js/mixedop.js
@@ -0,0 +1,8 @@
+var firstName = 'foyej';
+var lastName = 'khan';
+// console.log(firstName + ' ' + lastName)
+var result = firstName + ' ' + lastName;
+console.log(result)
+
+console.log('20'+10)
+console.log(40-'20')
