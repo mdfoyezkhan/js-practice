3bd1ad9d307dabb63521ac21009ea432fe42dcd9
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-06-03 23:23:24 +0600

    some operation code added

---
+++ repractice-js/operation.js
@@ -0,0 +1,23 @@
+// addition
+var price = 50;
+// price = price + 100;
+price += 100;
+console.log(price)
+// subtraction
+var phonePrice = 50;
+// phonePrice = phonePrice - 25;
+phonePrice -= 25;
+console.log(phonePrice)
+
+// multipication
+var applePrice = 50;
+// applePrice = applePrice * 5;
+applePrice *= 5;
+console.log(applePrice)
+// division
+var panPrice = 50;
+// panPrice = panPrice / 10;
+panPrice /= 10; 
+console.log(panPrice)
+
+
