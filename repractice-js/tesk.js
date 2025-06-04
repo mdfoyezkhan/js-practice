c58b73e75e75ef66f47e16be4287b475d7db9b98
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-06-04 22:19:56 +0600

    tesk practice code added in this commit

---
+++ repractice-js/tesk.js
@@ -0,0 +1,11 @@
+var orangesPrice = 500;
+console.log('oranges price',orangesPrice);
+var applesPrice = 200;
+console.log('apples price',applesPrice);
+var totalAmount = orangesPrice + applesPrice;
+console.log('total cost',totalAmount);
+var myMoney = 1000;
+console.log('my amount', myMoney);
+var beckMoney = myMoney - totalAmount;
+console.log('give money',beckMoney)
+
