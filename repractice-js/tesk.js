8dfad3052de3bd777ccaf009fee9133215416f8c
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-06-04 22:51:35 +0600

    result average code added in this commit

--- repractice-js/tesk.js
+++ repractice-js/tesk.js
@@ -9,3 +9,15 @@ console.log('my amount', myMoney);
 var beckMoney = myMoney - totalAmount;
 console.log('give money',beckMoney)
 
+// Write a program to calculate the average marks of Mathematics, Biology,
+//  Chemistry, Physics, and Bangla of a student. 75.25, 65, 80, 35.45, 99.50
+
+var Mathematics = 75.25;
+var Biology = 65;
+var Chemistry = 80;
+var Physics = 35.45;
+var bangla = 99.50;
+var totalResult = Mathematics + Biology + Chemistry + Physics + bangla;
+totalResult /= 5;
+console.log(totalResult)
+
