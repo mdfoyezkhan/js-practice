db1f966f415135afa7055f2641e823b825e2b7fa
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-06-18 21:46:20 +0600

    more comparison code added

--- repractice-js/modul-18/comparison.js
+++ repractice-js/modul-18/comparison.js
@@ -3,4 +3,14 @@
 // console.log(9 < 10);
 // console.log(10 >= 11);
 // console.log(10 <= 11);
-console.log(11 == 11);
+// console.log(11 == 11);
+// ----------------------
+
+var applesPrice = 200;
+var orangesPrice = 'tometo';
+// console.log(applesPrice > orangesPrice);
+// console.log(applesPrice >= orangesPrice);
+// console.log(applesPrice < orangesPrice);
+// console.log(applesPrice <= orangesPrice);
+// console.log(applesPrice == orangesPrice);
+console.log(applesPrice != orangesPrice);
