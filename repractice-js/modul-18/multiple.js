7d570859f92b42d890f1967010bb3c67381f6a30
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-06-18 22:50:22 +0600

    multiple condition code added

---
+++ repractice-js/modul-18/multiple.js
@@ -0,0 +1,16 @@
+const salary = 30000;
+const height = 90;
+const hasCar = true;
+// if(salary >25000 && height > 89){
+//     console.log('your perfect for boy')
+// }
+// else{
+//     console.log('getout from here')
+// }
+
+if((salary > 25000 && hasCar > true ) || height > 88){
+    console.log('hello world')
+}
+else{
+    console.log('hi everyone')
+}
