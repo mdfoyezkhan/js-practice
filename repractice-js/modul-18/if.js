f3e806f84f0e7178fcde37b702ce18d15d5fd414
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-06-18 22:19:31 +0600

    some condition code added

---
+++ repractice-js/modul-18/if.js
@@ -0,0 +1,35 @@
+if(5 > 3){
+    console.log('hello world')
+}
+var applesPrice = 300;
+if(applesPrice >= 300){
+    console.log('i will buy')
+}
+var orangesPrice = 200;
+if(orangesPrice < 500){
+    console.log('i have to buy')
+}
+var orangesPrice = 200;
+if(orangesPrice <= 500){
+    console.log('give me 1 kg')
+}
+var orangesPrice = 500;
+if(orangesPrice == 500){
+    console.log('give me 2 kg')
+}
+
+var bananaPrice = 400;
+if(bananaPrice > 400){
+    console.log('give me 1 dorjon banana')
+}
+else{
+    console.log('i dont want to buy')
+}
+
+var sugerPrice = 200;
+if(sugerPrice == 300){
+    console.log('i want to buy')
+}
+else{
+    console.log('i dont want to buy')
+}
