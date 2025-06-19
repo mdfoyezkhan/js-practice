9def83e8f5f51352dea33383c22a789e673e6bdc
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-06-19 21:32:22 +0600

    bmi code added

---
+++ repractice-js/modul-18/tesk2.js
@@ -0,0 +1,8 @@
+const weight = 59;
+const height = 5.3;
+const forInch = height * 12;
+const miter = forInch / 39.37;
+const sentimiter = miter * 2;
+const bmi =weight / sentimiter;
+
+console.log(bmi)
