4ec8585d5d9b2952af11136ea4b99202364ec60e
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-06-19 21:02:49 +0600

    great point ber korar code add kora holo

---
+++ repractice-js/modul-18/tesk.js
@@ -0,0 +1,13 @@
+// how to get gpa 
+let bangla = 84;
+bangla = 5;
+let politicalScienc = 57;
+politicalScienc = 3;
+let islamicHistory = 78;
+islamicHistory = 4;
+let philosophy = 63;
+philosophy = 3.5;
+const totalMark = bangla + politicalScienc + islamicHistory + philosophy;
+const totalSubject = 4;
+const getGpa = totalMark / totalSubject;
+console.log(getGpa)
