bd80c32da6e8e943a1f9b7572128a8da6dbe2dde
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-06-19 22:11:58 +0600

    nested condition code added

---
+++ repractice-js/modul-18/nested.js
@@ -0,0 +1,17 @@
+const money = -100;
+if(money > 300){
+    console.log('boss your a rich man!!')
+}
+else{
+    if(money > 200){
+        console.log('boss your a mediam rich man')
+    }
+    else{
+        if(money > 0){
+            console.log('boss your poor man ')
+        }
+        else{
+            console.log('soro amar dartaki')
+        }
+    }
+}
