4fcd474921e4c4be1905f7849078a6a45a072582
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-06-21 17:25:08 +0600

    nested tesk code added in this commit

---
+++ repractice-js/modul-18/tesk-for-friend.js
@@ -0,0 +1,23 @@
+const money = 85;
+if(money >= 80){
+   console.log('you can lunch with me')
+}
+else{
+    if(money > 60){
+        console.log('good luck next time')
+    }
+    else{
+        if(money > 40){
+            console.log('massage unseen')
+            
+        }
+        else{
+            if(money > 0){
+                console.log('your blocked')
+            }
+            else{
+                console.log('so sad i have to sleep know')
+            }
+        }
+    }
+}
