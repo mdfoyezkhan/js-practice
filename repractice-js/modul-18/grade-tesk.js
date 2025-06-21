c85831f219ec8f69de6fdb34e4600160cacf2cbb
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-06-21 16:38:50 +0600

    grade point tesk code added in this commit

---
+++ repractice-js/modul-18/grade-tesk.js
@@ -0,0 +1,9 @@
+let a = 4;
+let b = 3;
+let c = 2;
+let d = 1;
+let f = 0;
+const totalSquere = a + b + c + d + f;
+const getGradePoint = totalSquere / 4;
+console.log('Your Grade Point :', getGradePoint)
+
