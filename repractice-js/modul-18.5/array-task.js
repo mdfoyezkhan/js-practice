bb72c0ca70dd8e25df342a523cfce9d0c488a5cb
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-06-22 17:07:13 +0600

    array-task added

---
+++ repractice-js/modul-18.5/array-task.js
@@ -0,0 +1,4 @@
+const fruits = ['komola', 'apple', 'kola', 'pepe', 'katal'];
+console.log(fruits[3])
+fruits[2] = 'jambura';
+console.log(fruits)
---
+++ repractice-js/modul-18.5/includes.js
@@ -0,0 +1,18 @@
+const nam = ['foyej', 'salman', 'jobber', 'emon'];
+const num = [];
+const num2 = 121;
+console.log(nam.includes('sanam'))
+if(nam.includes('salman')){
+    console.log('i want to go jaflong with him')
+}
+else{
+    console.log('i dont go')
+}
+
+console.log(Array.isArray(nam))
+if(Array.isArray(num2)){
+    console.log('ami tumake kacci kawabo')
+}
+else{
+    console.log('tumi amake kacci kawabe')
+}
