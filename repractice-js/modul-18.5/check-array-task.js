11d211f97ef3dfb2dfa35013d5f11ebb3ad29f72
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-06-22 17:39:04 +0600

    array check task code added in this commit

---
+++ repractice-js/modul-18.5/check-array-task.js
@@ -0,0 +1,26 @@
+const num1 =[12, 23, 34, 45,];
+const num2 = [];
+const num3 = 123;
+console.log(Array.isArray(num1))
+if(Array.isArray(num1)){
+    console.log(typeof num1)
+}
+else{
+    console.log('not array')
+}
+
+console.log(Array.isArray(num2))
+if(Array.isArray(num2)){
+    console.log(typeof num2)
+}
+else{
+    console.log('not array')
+}
+
+console.log(Array.isArray(num3))
+if(Array.isArray(num3)){
+    console.log(typeof num3)
+}
+else{
+    console.log('not array')
+}
