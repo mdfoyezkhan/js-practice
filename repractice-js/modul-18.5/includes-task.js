099816c897a0ae18b7c80256dc0f91ab6d58e1fd
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-06-22 17:26:40 +0600

    includes-task code added in this commit

---
+++ repractice-js/modul-18.5/includes-task.js
@@ -0,0 +1,8 @@
+const bookList = ['bangla', 'english', 'logic', 'islamic history', 'javascript learnig'];
+console.log(bookList.includes('javascript learnig'))
+if(bookList.includes('javascript learnig')){
+    console.log('yes, i have javascript learnig book')
+}
+else{
+    console.log('i dont have javascript learning book')
+}
