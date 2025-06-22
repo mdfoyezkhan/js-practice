9b6a9588ed10a1e989c8834143d1af4de58e3255
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-06-22 16:29:45 +0600

    array push and pop code added in this commit

---
+++ repractice-js/modul-18.5/pop.js
@@ -0,0 +1,19 @@
+const numbers = [12, 23, 34, 45, 56, 67, 78, 89,90];
+const emty = [];
+emty.push(numbers)
+emty.pop()
+console.log(emty)
+console.log(numbers)
+numbers.push('foyej khan', 'salman khan')
+console.log(numbers)
+numbers.pop()
+numbers.shift()
+numbers.unshift(12)
+numbers.pop()
+console.log(numbers)
+const num = ['emon khan', 'jobbar khan'];
+numbers.push(num[0])
+numbers.push(num[1])
+
+console.log(numbers)
+
