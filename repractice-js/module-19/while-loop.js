5d62d8f8849e82c218da06994f0d9d824e3abe78
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-06-23 22:37:40 +0600

    while loop code added in this commit

---
+++ repractice-js/module-19/while-loop.js
@@ -0,0 +1,21 @@
+let num = 0;
+while(num <5 ){
+    console.log('hello world');
+    // num = num +1;
+    // num += 1;
+    const x = 1;
+    if(x <2){
+        console.log('hi i am if condition')
+    }
+    else{
+        console.log('sorry !')
+    }
+    num++;
+}
+console.log('hello')
+
+let friend = true;
+while(friend == true){
+    console.log('hi i am true');
+    return friend;
+}
