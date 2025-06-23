76365b7a3043266f7d9f7ad4db67b76875832f13
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-06-23 23:16:17 +0600

    while loop fot problen solving code added

---
+++ repractice-js/module-19/problem-solving.js
@@ -0,0 +1,21 @@
+// let num = 1;
+// while(num <= 10){
+//     console.log(num);
+//     num++;
+// }
+// let num = 1;
+// let sum = 0;
+// while(num <= 10){
+//     sum = sum + num;
+//     console.log('hello world' , sum)
+//     num++;
+// }
+let num = 1;
+while(num <= 10) {
+    console.log(num)
+    if(num % 2 === 1){
+        console.log('even number', num)
+
+    }
+    num++;
+}
