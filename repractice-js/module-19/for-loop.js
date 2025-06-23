9842fd8ea767a06ce4cc901f38de78f9c718f2fd
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-06-24 00:11:18 +0600

    some code added for for loop and while loop

---
+++ repractice-js/module-19/for-loop.js
@@ -0,0 +1,14 @@
+let num = 10;
+while(num > 5){
+    console.log(num)
+    num--;
+}
+for(num = 0; num <= 5; num++){
+    console.log(num, 'for loop')
+}
+for(num = 0; num <= 10; num = num + 2){
+    console.log(num, 'even number')
+}
+for(num = 11; num <= 20; num = num + 2){
+    console.log(num, 'odd number')
+}
---
+++ repractice-js/module-19/while.js
@@ -0,0 +1,35 @@
+// let workinDay = 1;
+// while(workinDay <= 60){
+//     console.log(workinDay ,'"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.');
+//     workinDay++;
+// }
+
+// let oddNumber = 61;
+// while(oddNumber <= 100){
+//     if(oddNumber % 2 === 1){
+//         console.log(oddNumber, 'odd number');
+//     }
+//     oddNumber++;
+// }
+// let evenNumber = 78;
+// while(evenNumber <= 98){
+//     if(evenNumber % 2 === 0){
+//         console.log(evenNumber, 'even number')
+//     }
+//     evenNumber++;
+// }
+
+let odd = 81;
+let sum = 0;
+while(odd <= 131){
+    sum = sum + odd;
+    console.log(sum, 'all of odd number')
+    odd++;
+}
+let even = 206;
+let sum2 = 0;
+while(even < 311){
+    sum2 = sum2 + even;
+    console.log(sum2, 'all of even number');
+    even++;
+}
