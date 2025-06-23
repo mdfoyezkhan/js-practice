1d19b488debf3383e4954df279ca8a72c57ec12d
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-06-23 21:58:50 +0600

    for loop code added in this commit for loop added in this commit

---
+++ repractice-js/module-19/loop.js
@@ -0,0 +1,11 @@
+const numbers = [12, 23, 34, 45, 56, 67];
+console.log(numbers)
+for(const number of numbers){
+    console.log(number)
+}
+const friends = ['salman', 'emon', 'sanam', 'arman', 'samaun'];
+console.log(friends);
+for(const friend of friends){
+    console.log(friend);
+    // console.log(friend[1])
+}
