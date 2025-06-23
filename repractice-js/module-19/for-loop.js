bae1c6df6637432bffddb564ee28ae0aaa5d145e
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-06-24 00:19:21 +0600

    some code added in this commit

--- repractice-js/module-19/for-loop.js
+++ repractice-js/module-19/for-loop.js
@@ -3,12 +3,12 @@ while(num > 5){
     console.log(num)
     num--;
 }
-for(num = 0; num <= 5; num++){
+for(let num = 0; num <= 5; num++){
     console.log(num, 'for loop')
 }
-for(num = 0; num <= 10; num = num + 2){
+for(let num = 0; num <= 10; num = num + 2){
     console.log(num, 'even number')
 }
-for(num = 11; num <= 20; num = num + 2){
+for(let num = 11; num <= 20; num = num + 2){
     console.log(num, 'odd number')
 }
