fca09829a6e9c98b526f069dc547f5dba4df2f45
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-06-24 22:26:16 +0600

    different loop added

---
+++ repractice-js/module-19/different.js
@@ -0,0 +1,20 @@
+// for(let i = 10; i < 20 ; i++){
+//     if( i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+// console.log('total')
+// for(let i = 0; i < 20; i++){
+//     if(i % 2 === 1){
+//         console.log(i)
+//     }
+// }
+let sum = 0;
+for(let i = 0; i < 10; i++){
+    if(i % 2 === 0){
+        console.log(sum);
+        sum = sum + i;
+    }
+
+}
+console.log('total', sum)
