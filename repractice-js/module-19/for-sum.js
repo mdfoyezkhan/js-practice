339f376beef22c9d517ce12fac52e8353585c8fd
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-06-24 21:56:58 +0600

    for sum loop added

---
+++ repractice-js/module-19/for-sum.js
@@ -0,0 +1,13 @@
+// let sum = 0;
+// for(let i = 10; i > 0; i-- ){
+//     sum = sum + i;
+//     // console.log(sum);
+
+// }
+//  console.log(sum);
+ let num = 10;
+ while(num > 0){
+    console.log(num);
+    num--;
+ }
+
