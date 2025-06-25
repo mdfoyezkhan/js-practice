faad578c5de33455534e8274233b68ffee4f763c
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-06-25 23:54:38 +0600

    break task added

--- repractice-js/module-19/break-task.js
+++ repractice-js/module-19/break-task.js
@@ -8,9 +8,46 @@
     
     
 // }
-for(let i = 1; i < 200; i++){
-    if(i > 100){
+// for(let i = 1; i < 200; i++){
+//     if(i > 100){
+//         break;
+//     }
+//     console.log(i,'break')
+// }
+
+
+// let sum = 0;
+// let x = 1;
+// while(x < 200){
+//     sum = sum + x;
+     
+//     if(sum >= 100){
+//         break;
+//     }
+//    x++;
+    
+// }
+// console.log(sum)
+
+// for( let i = 1; i < 100; i++){
+//     if(Math.sqrt(i) % 2 == 0){
+//         break
+//     }
+//     console.log(i)
+// }
+// for(let x = 5; x < 100; x++){
+//     if(Math.sqrt(x) % 2 === 1){
+//         break;
+//     }
+//     console.log(x)
+// }
+
+let n = 10;
+while( n < 100){
+    n++;
+    if(Math.sqrt(n) % 2 === 0){
         break;
     }
-    console.log(i,'break')
+    console.log(n)
 }
+
