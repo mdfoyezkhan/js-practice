356dec7d2bf5ab1c03f55015ba840429ea7a0276
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-06-25 22:42:00 +0600

    do while code added

---
+++ repractice-js/module-19/do-while.js
@@ -0,0 +1,24 @@
+// let x = 0;
+// while( x <= 5){
+//     console.log(x)
+//     x++;
+// }
+
+// console.log('do while start here');
+
+// let n = 0;
+// do { 
+//     console.log(n);
+//     n++;
+// }
+// while(n < 5){
+//     console.log(n)
+// }
+let y = 30;
+do {
+    console.log(y);
+    y++;
+}
+while(y < 50){
+    console.log(y ,'hello world')
+}
