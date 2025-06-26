c8e0c9b4919909e33f8f87fdddc4815840ae39e4
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-06-26 21:38:21 +0600

    all task for and while loop added

---
+++ repractice-js/module-19/all-task/break.js
@@ -0,0 +1,14 @@
+for(let i = 1; i < 200; i++){
+    if(i > 100){
+        break;
+    }
+    console.log(i)
+}
+let i = 1;
+while(i < 200){
+    i++;
+    if(i > 100){
+        break;
+    }
+    console.log(i, 'while loop')
+}
