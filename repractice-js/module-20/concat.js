7ad649e1c4635047a8010c169207bb718609409b
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-07-01 23:47:09 +0600

    concat.js file added

---
+++ repractice-js/module-20/concat.js
@@ -0,0 +1,17 @@
+// const firstName = 'foyej';
+// const lastName = 'khan';
+// console.log(firstName + lastName);
+// console.log(firstName.concat(' ').concat(lastName))
+const firstName = 'salman';
+const lastName = 'khan';
+const lastName1 = 'my friend';
+console.log(firstName.concat(lastName));
+console.log(firstName.concat(' ').concat(lastName).concat(' ').concat(lastName1));
+const x = 'how are you';
+console.log(x.includes('o'))
+if(x.includes('h')){
+    console.log('ase')
+}
+else{
+    console.log('nai')
+}
