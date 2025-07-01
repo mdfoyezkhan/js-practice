f7107974673ee1af91fae6e9266145c0246ea49c
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-07-01 22:35:42 +0600

    case.js file added

---
+++ repractice-js/module-20/case.js
@@ -0,0 +1,41 @@
+// const school = 'Brindabon Govt college';
+// console.log(school);
+// console.log(school.toLowerCase());
+// console.log(school.toUpperCase())
+// const firstName = 'foyej';
+// const lastName = 'Foyej';
+// if(firstName.toUpperCase() === lastName.toUpperCase()){
+//     console.log('you are login')
+// }
+// else{
+//     console.log('incurat')
+// }
+
+// const x = 'foyej';
+// const y = ' foyej';
+// if(x.trim === y.trim){
+//     console.log('hello')
+// }
+// else{
+//     console.log('try agein')
+// }
+const school = 'Brindabon Govt college';
+console.log(school);
+console.log(typeof school);
+console.log(school.length);
+const firstName = 'foyej';
+const lastName = 'foYej';
+if(firstName.toUpperCase() === lastName.toUpperCase()){
+    console.log('you first and last name is some')
+}
+else{
+    console.log('your first and last nam not some')
+}
+const x = 'foyej ';
+const y = ' foyej';
+if(x.trim === y.trim){
+    console.log('your login')
+}
+else{
+    console.log('try agein')
+}
