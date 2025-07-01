8bdf46616dad45ffa7a1741d399e3b96ab0b7914
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-07-01 21:58:31 +0600

    module-20  array.js file added

---
+++ repractice-js/module-20/array.js
@@ -0,0 +1,11 @@
+const number = [12, 23, 34, 45, 56, 67, 78];
+console.log(number.length)
+const cuntry = 'bangladesh';
+console.log(cuntry.length)
+// console.log(cuntry[2])
+const friend = new String('salman');
+console.log(typeof friend)
+const kobita =`hi 
+how are you 
+`;
+console.log(kobita)
