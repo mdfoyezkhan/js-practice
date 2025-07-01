667b2d33bcf698b0184203f0d6ef0458b5d10027
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-07-01 23:31:59 +0600

    slice.js file added

---
+++ repractice-js/module-20/slice.js
@@ -0,0 +1,29 @@
+// const name = 'foyejkhan';
+// const fullName = name.slice(5, 9);
+// console.log(fullName);
+// const sentence = 'i am a good and hardworking man';
+// console.log(sentence.split(''))
+// console.log(sentence.split(' '))
+// console.log(sentence.split('a'))
+// console.log(sentence.split(','))
+// const friend = 'foyej,salman,emon,jobbar';
+// const friends = friend.split(',')
+// console.log(friends);
+// const bondu = ['foyej', 'salman', 'emon', 'jobbar'];
+// console.log(bondu.join(''))
+// console.log(bondu.join('|'))
+// console.log(bondu.join('-'))
+const name = 'foyejkhan';
+const firstName = name.slice(0, 5);
+const lastName = name.slice(5, 9)
+const fullName = firstName + ' ' + lastName;
+console.log(fullName);
+const sentence = 'i am a good and hardworking man';
+// console.log(sentence.split(''));
+// console.log(sentence.split(' '));
+console.log(sentence.split('h'));
+const friend =['foyej', 'salman', 'emon', 'jobbar'];
+console.log(friend.join())
+console.log(friend.join('|'))
+console.log(friend.join('-'))
+
