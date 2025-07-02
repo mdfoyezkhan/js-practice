f5340e91950a9b0a45095574b7f701cb6acfbe40
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-07-02 23:09:45 +0600

    property.js file added

---
+++ repractice-js/module-20/property.js
@@ -0,0 +1,29 @@
+const person ={
+    name: 'foyej khan',
+    age: 23,
+    'blod group':'B posetive',
+    profession:'suport staf',
+    salary: 15000,
+    workAt:'vivasoft',
+    myCompany:{
+        companyName:'vivasoft',
+        employ: 200,
+        workTime: ' 10am to 7pm',
+    }
+}
+
+console.log(person.myCompany)
+let Company = person.myCompany.employ;
+Company= 230;
+console.log(Company)
+// person.age = 25;
+// console.log(person.age)
+// person['profession']= 'developer';
+// console.log(person.profession)
+// person['blod group'] = 'A posetive';
+// console.log(person['blod group'])
+// console.log(person);
+// console.log(person.salary)
+// const mathlySalary = person.salary;
+// console.log(mathlySalary)
+// console.log(person['blod group'])
