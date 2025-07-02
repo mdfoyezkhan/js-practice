a13723c5b304c6f14f5e39d1da00d259b2f416d6
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-07-02 22:27:11 +0600

    object.js file added

---
+++ repractice-js/module-20/object.js
@@ -0,0 +1,21 @@
+const subject ={
+    subjectName: 'information and cominication technology',
+    teacher: 'babu sir',
+    chapter: ['first', 'secend', 'thred'],
+    exam: '29 jul',
+}
+console.log(subject);
+
+const leptop ={
+    brand:'hp',
+    color:['gree', 'black', 'white'],
+    size: '14inch',
+    isbrandNew:'brand new',
+    howManeyBrand:{
+        hp:'zBook',
+        dell:'dell latitute',
+        asus:'asus',
+    },
+    camera:'50mp',
+}
+console.log(leptop)
