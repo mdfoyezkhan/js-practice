b5f02b0afddde71728093211a409792960e66ee0
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-07-07 23:06:36 +0600

    return.js file added

---
+++ repractice-js/module-21/return.js
@@ -0,0 +1,16 @@
+function doubel(number){
+const digun = number * 2;
+return digun;
+}
+doubel(10)
+const outPut = doubel(10);
+console.log(outPut)
+
+function difference(num1, num2){
+const diff = num1 - num2;
+return diff;
+}
+const fotherAge = 55;
+const motherAge = 45;
+const differenceAge = difference(fotherAge , motherAge)
+console.log(differenceAge)
