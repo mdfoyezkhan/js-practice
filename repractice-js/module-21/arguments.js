1942a4eb430a2cd4749fac21e9b590498ad2cd0e
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-07-07 22:44:49 +0600

    arguments.js file added

---
+++ repractice-js/module-21/arguments.js
@@ -0,0 +1,20 @@
+function digun(man){
+   const doubel = man * 2;
+   console.log( man, 'digun' , doubel)
+}
+digun(10)
+digun(20)
+digun(30)
+digun(40)
+console.log('----------------------------')
+function difference(num1, num2){
+    const diff = num1 - num2;
+    console.log(diff)
+
+}
+let fotherAge = 50;
+let myAge = 23;
+difference(fotherAge, myAge)
+console.log('-------------------------')
+let motherAge = 40;
+difference(motherAge, myAge)
