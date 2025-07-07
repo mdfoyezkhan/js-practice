e5c968c4dbc691a4e16b9c42b9fe655a82c021b0
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-07-07 22:20:24 +0600

    parameter.js file added

---
+++ repractice-js/module-21/parameter.js
@@ -0,0 +1,26 @@
+const number = 50;
+const num = 50;
+function squre (number, num){
+    // console.log(number)
+    const borgo = number + num;
+    console.log(borgo)
+}
+squre(number, num);
+
+
+function fullName(first, last){
+    const firstAndLastAdded = first + ' ' + last;
+    console.log(firstAndLastAdded)
+}
+fullName('foyej', 'khan')
+
+function allSqure(number){
+const sum = number * number;
+console.log(sum)
+}
+allSqure(10)
+allSqure(11)
+allSqure(12)
+allSqure(13)
+allSqure(50)
+
