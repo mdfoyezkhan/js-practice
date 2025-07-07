b6d491346f7070aff6cfbee8ad1c45928f9513c4
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-07-07 21:38:00 +0600

    function.js file added

---
+++ repractice-js/module-21/function.js
@@ -0,0 +1,17 @@
+const price = 59;
+function money (){
+    console.log('hello world')
+}
+money();
+
+function myFriend (){
+    console.log('hello my friend');
+}
+myFriend();
+
+console.log('---------------------')
+console.log('---------------------')
+console.log('---------------------')
+
+money();
+myFriend();
