7f84c041726616767def49cdbcff2427f90c298c
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-07-11 06:26:48 +0600

    math.js file added

---
+++ repractice-js/module-22/math.js
@@ -0,0 +1,13 @@
+console.log(Math.min(12, 34,56,67,78,));
+console.log(Math.max(12, 34,56,67,78,));
+console.log(Math.PI);
+console.log(Math.abs(10 - 5));
+console.log(Math.abs(5 - 10));
+console.log(Math.round(4.5));
+console.log(Math.round(4.4));
+console.log(Math.floor(4.9));
+console.log(Math.ceil(4.2));
+console.log(Math.random())
+console.log(Math.random() * 10);
+const remd = Math.round(Math.random() * 10);
+console.log(remd)
