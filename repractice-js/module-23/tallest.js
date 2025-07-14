a1d5e08c35b7096a1d4a04f74ed0766481b45f44
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-07-14 23:25:44 +0600

    some code change in tallest.js file

--- repractice-js/module-23/tallest.js
+++ repractice-js/module-23/tallest.js
@@ -2,8 +2,8 @@ const minNumber = [4, 23, 24, 34, 45, 23, 45, 67, 12, 45, 11, 6];
 function forMinNumber(numbers){
     let forMin = numbers[0];
     for(const number of numbers){
-        if(forMin > number){
-            // forMin = number;
+        if(number < forMin){
+            forMin = number;
         }
     }
     return forMin;
