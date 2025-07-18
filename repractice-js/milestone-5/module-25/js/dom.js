28e747f1b679edfd76be61fdbf75f5f1d21cd5c8
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-07-18 23:41:29 +0600

    some code added

--- repractice-js/milestone-5/module-25/js/dom.js
+++ repractice-js/milestone-5/module-25/js/dom.js
@@ -1,17 +1,20 @@
-let htmlCollectionForLi = document.getElementsByTagName('li');
-// console.log(htmlCollectionForLi);
-for(const hcLi of htmlCollectionForLi){
-    console.log(hcLi.innerText)
-}
+// let htmlCollectionForLi = document.getElementsByTagName('li');
+// // console.log(htmlCollectionForLi);
+// for(const hcLi of htmlCollectionForLi){
+//     console.log(hcLi.innerText)
+// }
 
-let forId = document.getElementById('Lorem1');
-forId = innerText = 'hello world';
-console.log(forId.innerHTML='<h1>hello world</h1>');
-// const forId1 = document.getElementById('Lorem1');
-// console.log(forId1);
-const forClass =document.getElementsByClassName('ipsum');
-console.log(forClass)
-for(const foyez of forClass){
-    console.log(foyez)
-}
+// let forId = document.getElementById('Lorem1');
+// forId = innerText = 'hello world';
+// console.log(forId.innerHTML='<h1>hello world</h1>');
+// // const forId1 = document.getElementById('Lorem1');
+// // console.log(forId1);
+// const forClass =document.getElementsByClassName('ipsum');
+// console.log(forClass)
+// for(const foyez of forClass){
+//     console.log(foyez)
+// }
 // const collection = document.getElementsByClassName("example");
+const getId = document.getElementById('Lorem1');
+console.log(getId)
+
