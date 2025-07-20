f4a4ac9fd79aa3a92013af5e2ab43647a7f4640f
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-07-20 18:52:42 +0600

    javascript dom events heandler added

---
+++ repractice-js/milestone-5/module-26/js/events.js
@@ -0,0 +1,10 @@
+console.log('this file is made for javascript code practice')
+function bgRedButton() {
+    document.body.style.background = 'red';
+}
+
+const bgBlue = document.getElementById('bgblue');
+bgBlue.onclick = bgBlue1;
+function bgBlue1() {
+    document.body.style.backgroundColor = 'blue';
+}
