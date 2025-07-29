349ea33ca13ba081fd471b6ae5fc2b9a34bd1220
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-07-29 22:31:37 +0600

    more arraw.js file and destructure.js file added

---
+++ repractice-js/milestone-6/module-30/destructure.js
@@ -0,0 +1,20 @@
+const student = {
+    name: 'foyej',
+    age: 23,
+    phone:'01771944422',
+    subject:'BA'
+}
+// const {name, age, phone ,subject} = student;
+// console.log(name);
+// console.log(age);
+// console.log(phone);
+// console.log(subject);
+const name = student.name;
+const age = student.age;
+const phone = student.phone;
+const subject = student.subject;
+console.log(name);
+console.log(age);
+console.log(phone);
+console.log(subject);
+
---
+++ repractice-js/milestone-6/module-30/more-arraw.js
@@ -0,0 +1,16 @@
+const deffrent = (person) => a + b;
+const student = {
+    name: 'foyej',
+    age: 23,
+}
+const person = (person) => person.age
+console.log(person(student))
+
+const sum = (a, b, c) => {
+    const x = a + b + c;
+    const y = a - b -c;
+    const z = a * b * c;
+    return x, y, z;
+}
+const result = sum(12, 12, 12);
+console.log(result)
