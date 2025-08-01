ec00661bdb84431b2f8b0b34d9dc078f99e434c9
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-08-01 23:18:31 +0600

    equal.js file added

---
+++ repractice-js/milestone-6/module-32/equal.js
@@ -0,0 +1,18 @@
+const first = 0;
+const first1 = false;
+if (first == first1){
+    console.log('value are equal')
+}
+else{
+    console.log('value are not equal')
+}
+
+
+const x = [12,23,34,];
+const  y= [12,23,34,];
+if(x == y){
+    console.log('value is equal')
+}
+else{
+    console.log('value is not equal')
+}
