7e1211b311e56d70d06f87ea3067bb6e7d449285
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-08-01 22:50:31 +0600

    true-false.js file added j

---
+++ repractice-js/milestone-6/module-32/true-false.js
@@ -0,0 +1,24 @@
+const x = false;
+if (x){
+    console.log('value is truthy')
+}
+else{
+    console.log('value is falsey')
+}
+
+const y = false;
+if (!y){
+    console.log('value is truthy')
+}
+else{
+    console.log('value is falsey')
+}
+
+const b = null;
+if (b){
+    console.log('value is truthy')
+}
+else{
+    console.log('value is falsey')
+}
+// console.log(x)
