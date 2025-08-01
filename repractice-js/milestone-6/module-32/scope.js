58599ba6123510cdca0556e03841cef228f7941e
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-08-01 23:33:18 +0600

    scope.js file added

---
+++ repractice-js/milestone-6/module-32/scope.js
@@ -0,0 +1,12 @@
+function scopes(a, b){
+
+    const total= a + b;
+    if( b > 5){
+        const sum = a + b;
+    }
+    else{
+        const sum = a - b;
+    }
+    return total;
+}
+console.log()
