37153f11f78dc144f3725b645af69c0933afe4b0
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-08-09 23:05:01 +0600

    some new file added

--- repractice-js/milestone-6/module-33/js/comments.js
+++ repractice-js/milestone-6/module-33/js/comments.js
@@ -15,3 +15,29 @@ const getComments3 = async() =>{
     const data = await res.json();
     console.log(data)
 }
+
+const forPhotos = ()=>{
+//    const getPhoto = fetch('https://jsonplaceholder.typicode.com/comments');
+//    const res = getPhoto.text()
+//    console.log(res)
+fetch('https://jsonplaceholder.typicode.com/photos')
+.then(res => res.json())
+.then(data => console.log(data))
+}
+
+const photos = async()=>{
+    const res =await fetch('https://jsonplaceholder.typicode.com/photos');
+    const datas =await res.json();
+    getSengelePhoto(datas)
+}
+const getSengelePhoto = (datas) =>{
+    const ul =document.getElementById('ul');
+    for(const data of datas ){
+        const li = document.createElement('li');
+        li.innerText = data.title;
+        li.classList.add('cssstyle')
+        li.classList.add('gred')
+        ul.appendChild (li)
+
+    }
+}
---
+++ repractice-js/milestone-6/module-34/script.js
@@ -0,0 +1,21 @@
+// const x = 10;
+// function y (num1, num2){
+//     const sum = num1 + num2 + x;
+//     return sum;
+// }
+//  const n = y(10, 10)
+//  console.log(n)
+
+console.log(1)
+console.log(2)
+// fetch('https://jsonplaceholder.typicode.com/photos')
+// .then(res => res.json())
+// .then(data => console.log(data))
+setTimeout(()=>{
+    console.log('i am come form set time out')
+}, 2000)
+console.log(4 )
+console.log(5)
+function x (){
+    console.log(3)
+}
